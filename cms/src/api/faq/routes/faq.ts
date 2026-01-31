abe3bf5249d7f11178a1603c8485ae1b1eeab13a
Alexander Safronov <aleks.v.safronov@gmail.com>
2026-01-31 23:32:19 +0100

    fix: Add TypeScript ignore comments for FAQ content type
    - Add @ts-ignore to FAQ controller, service, and routes
    - Fixes TypeScript compilation errors during build
    - Content type definitions will be generated after first build

--- cms/src/api/faq/routes/faq.ts
+++ cms/src/api/faq/routes/faq.ts
@@ -4,4 +4,5 @@
 
 import { factories } from '@strapi/strapi';
 
+// @ts-ignore - Content type will be available after build
 export default factories.createCoreRouter('api::faq.faq');
--- cms/src/api/faq/services/faq.ts
+++ cms/src/api/faq/services/faq.ts
@@ -4,4 +4,5 @@
 
 import { factories } from '@strapi/strapi';
 
+// @ts-ignore - Content type will be available after build
 export default factories.createCoreService('api::faq.faq');
