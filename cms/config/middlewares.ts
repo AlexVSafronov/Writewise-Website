7ed0043dab74e85bb66aec907c6fb26476cef564
Alexander Safronov <aleks.v.safronov@gmail.com>
2026-02-11 08:16:34 +0100

    fix: Add both Cloud Run URL formats to CORS (old and new)
    - Old format: -m2xkjyh6ta-oe.a.run.app
    - New format: -918249600328.europe-west10.run.app

--- cms/config/middlewares.ts
+++ cms/config/middlewares.ts
@@ -6,7 +6,14 @@ export default [
     name: 'strapi::cors',
     config: {
       enabled: true,
-      origin: ['*'], // Allow all origins for now
+      origin: [
+        'http://localhost:8081',
+        'http://localhost:5173',
+        'https://write-wise.com',
+        'https://writewise-website-m2xkjyh6ta-oe.a.run.app',
+        'https://writewise-website-918249600328.europe-west10.run.app',
+        'https://*.vercel.app'
+      ],
       methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
       headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
       keepHeaderOnError: true,
